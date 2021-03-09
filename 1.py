e4506cf17cb945a5de72271cc3dcd1958c105cfa
ali sojoudizadeh <sojoudizadehali@gmail.com>
2021-03-10 00:10:55 +0330

    V2

--- 1.py
+++ 1.py
@@ -2,6 +2,6 @@ from datetime import datetime
 odds=[1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49,51,53,55,57,59]
 right_this_min=datetime.today().minute
 if right_this_min in odds:
-    print("odd min")
+    print("the min is {} => odd min".format(right_this_min))
 else:
-    print("not an odd") 
+    print("the min is {} => not odd min".format(right_this_min))

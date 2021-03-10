259a3792c9f76405fc3e2f02c262692dfa0f86ed
ali sojoudizadeh <49396867+alisojoudizadeh@users.noreply.github.com>
2021-03-11 01:36:26 +0330

    Delete 3.1.py

--- 3.1.py
+++
@@ -1,8 +0,0 @@
-phrase="!Salam Va vaght bekheyr !"
-ph_list=list(phrase)
-print(phrase)
-print(ph_list)
-new_phrase="".join(ph_list[1:3])
-new_phrase=new_phrase+"".join([phrase[18],ph_list[19],ph_list[14],ph_list[8],ph_list[22]])
-print(ph_list)
-print(new_phrase)
